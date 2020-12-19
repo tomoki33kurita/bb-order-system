3679e06ade1f9a2b117a70ac3227cba172c553f3
tomoki33kurita <tomoki33kurita@gmail.com>
2020-12-19 17:01:36 +0900

    Implemented base component

---
+++ src/container/Designation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+
+const Designation: React.FC = () => {
+  return (
+    <Box m={1} p={5} border={'solid 1px green'}>
+      ここがオーダー内容指定エリア
+    </Box>
+  )
+}
+export default Designation
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
+import { Box, Grid } from '@material-ui/core'
+import FigureFields from 'src/container/FigureFields'
+import Designation from 'src/container/Designation'
 
-const Index: React.FC = () => <div>コマネチ</div>
+const Index: React.FC = () => {
+  return (
+    <Box>
+      ハタケヤマ オーダーシュミレーション
+      <Grid container>
+        <Grid item md={6}>
+          <FigureFields />
+        </Grid>
+        <Grid item md={6}>
+          <Designation />
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
 
 export default Index
