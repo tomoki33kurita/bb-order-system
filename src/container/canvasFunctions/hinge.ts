483b814a6e2e1a56144ead9e9e472968283eb708
tomoki33kurita <tomoki33kurita@gmail.com>
2020-12-29 20:17:56 +0900

    Implement leather strap around thumb

--- src/container/canvasFunctions/hinge.ts
+++ src/container/canvasFunctions/hinge.ts
@@ -1,7 +1,7 @@
 // 土手外側
 export const hingeUnderLine = (ctx: CanvasRenderingContext2D): void => {
   ctx.beginPath()
-  ctx.moveTo(450, 605)
+  ctx.moveTo(465, 600)
   ctx.lineTo(700, 520)
   ctx.closePath()
   ctx.stroke()
--- src/container/canvasFunctions/leatherStrap.ts
+++ src/container/canvasFunctions/leatherStrap.ts
@@ -64,6 +64,60 @@ export const leatherStrap = (ctx: CanvasRenderingContext2D): void => {
   ctx.moveTo(445, 145)
   ctx.lineTo(450, 160)
 
-  // ctx.quadraticCurveTo(470, 520, 500, 588)
+  // ウェブと親指芯
+  ctx.moveTo(205, 125)
+  ctx.quadraticCurveTo(220, 90, 255, 90)
+  ctx.moveTo(290, 120)
+  ctx.lineTo(245, 130)
+  ctx.moveTo(295, 140)
+  ctx.lineTo(265, 150)
+  ctx.moveTo(300, 170)
+  ctx.lineTo(280, 175)
+
+  // ウェブと小指芯先端
+  ctx.moveTo(490, 25)
+  ctx.quadraticCurveTo(525, 30, 525, 40)
+  ctx.moveTo(475, 70)
+  ctx.lineTo(520, 60)
+  ctx.moveTo(480, 95)
+  ctx.lineTo(520, 87)
+  ctx.moveTo(490, 120)
+  ctx.lineTo(520, 110)
+
+  // ウェブ下捕球面と親指下捕球面
+  ctx.moveTo(295, 220)
+  ctx.lineTo(310, 210)
+
+  // ウェブ下捕球面と小指先端下捕球面
+  ctx.moveTo(500, 155)
+  ctx.lineTo(525, 158)
+
+  // 親指芯周り
+  ctx.moveTo(165, 155)
+  ctx.lineTo(195, 160)
+  ctx.moveTo(166, 195)
+  ctx.lineTo(196, 200)
+  ctx.moveTo(172, 235)
+  ctx.lineTo(202, 240)
+  ctx.moveTo(180, 275)
+  ctx.lineTo(210, 280)
+  ctx.moveTo(186, 310)
+  ctx.lineTo(218, 315)
+  ctx.moveTo(197, 350)
+  ctx.lineTo(227, 355)
+  ctx.moveTo(216, 390)
+  ctx.lineTo(246, 395)
+  ctx.moveTo(235, 430)
+  ctx.lineTo(272, 435)
+  ctx.moveTo(261, 470)
+  ctx.lineTo(301, 473)
+  ctx.moveTo(290, 505)
+  ctx.lineTo(330, 507)
+  ctx.moveTo(320, 535)
+  ctx.lineTo(360, 535)
+  ctx.moveTo(353, 562)
+  ctx.lineTo(388, 560)
+  ctx.moveTo(394, 585)
+  ctx.lineTo(429, 580)
   ctx.stroke()
 }
