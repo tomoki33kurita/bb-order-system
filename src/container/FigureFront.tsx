731914a487c9685dc76d3af251726ebbf6ef4d0f
tomoki33kurita <tomoki33kurita@gmail.com>
2020-12-30 14:52:36 +0900

    Applied thumb color

--- src/container/FigureFront.tsx
+++ src/container/FigureFront.tsx
@@ -1,13 +1,5 @@
 import React from 'react'
-import {
-  thumbTopOutLine,
-  thumbTopInLine,
-  thumbOutLine,
-  thumbEdgeLine1,
-  thumbEdgeLine2,
-  thumbInLine1,
-  thumbInLine2,
-} from 'src/container/canvasFunctions/thumb'
+import { thumbMachi, thumbTarget } from 'src/container/canvasFunctions/thumb'
 import { littleOutLine, littleInLineBottom, littEdgeLine1, littEdgeLine2, littEdgeLine3 } from 'src/container/canvasFunctions/little'
 import { littleTopLeftLine, littleTopOutLine } from 'src/container/canvasFunctions/littleTop'
 import { web, webTop } from 'src/container/canvasFunctions/web'
@@ -18,7 +10,6 @@ import {
   underWebRightLine,
   underWebRightRightLine,
 } from 'src/container/canvasFunctions/catchingSurface'
-import { hingeUnderLine } from 'src/container/canvasFunctions/hinge'
 import { leatherStrap } from 'src/container/canvasFunctions/leatherStrap'
 import { edgeLeather } from 'src/container/canvasFunctions/edge'
 
@@ -41,32 +32,27 @@ const FigureFront: React.FC<Props> = ({ leatherStrapColor, webColor }) => {
     ctx.strokeStyle = '#383838'
     ctx.lineWidth = 2
 
-    // ウェブ下捕球面
-    underWebTopLine(ctx)
-    underWebLeftLine(ctx)
-    underWebLeftLeftLine(ctx)
-    underWebRightLine(ctx)
-    underWebRightRightLine(ctx)
-    // 親指芯
-    thumbTopOutLine(ctx)
-    thumbTopInLine(ctx)
-    thumbOutLine(ctx)
-    thumbEdgeLine1(ctx)
-    thumbEdgeLine2(ctx)
-    thumbInLine1(ctx)
-    thumbInLine2(ctx)
-    // 小指芯
-    littleOutLine(ctx)
-    littEdgeLine1(ctx)
-    littEdgeLine2(ctx)
-    littEdgeLine3(ctx)
-    littleInLineBottom(ctx)
-    // 小指芯先端
-    littleTopLeftLine(ctx)
-    littleTopOutLine(ctx)
+    // // ウェブ下捕球面
+    // underWebTopLine(ctx)
+    // underWebLeftLine(ctx)
+    // underWebLeftLeftLine(ctx)
+    // underWebRightLine(ctx)
+    // underWebRightRightLine(ctx)
+    // // 小指芯
+    // littleOutLine(ctx)
+    // littEdgeLine1(ctx)
+    // littEdgeLine2(ctx)
+    // littEdgeLine3(ctx)
+    // littleInLineBottom(ctx)
+    // // 小指芯先端
+    // littleTopLeftLine(ctx)
+    // littleTopOutLine(ctx)
     // ヘリ革
     edgeLeather(ctx, webColor)
-    // ウェブ
+    // 親指マチ部分
+    thumbMachi(ctx, webColor)
+    thumbTarget(ctx, webColor)
+    // // ウェブ
     web(ctx, webColor)
     webTop(ctx, webColor)
     // 革紐
--- src/container/canvasFunctions/thumb.ts
+++ src/container/canvasFunctions/thumb.ts
@@ -1,43 +1,8 @@
-// 親指上部左
-export const thumbTopOutLine = (ctx: CanvasRenderingContext2D): void => {
-  ctx.beginPath()
-  ctx.moveTo(175, 133)
-  ctx.quadraticCurveTo(180, 125, 225, 120)
-  ctx.stroke()
-}
-
 // 親指上部右
 export const thumbTopInLine = (ctx: CanvasRenderingContext2D): void => {
   ctx.beginPath()
   ctx.moveTo(225, 120)
   ctx.quadraticCurveTo(270, 145, 275, 170)
-  ctx.stroke()
-}
-
-// 親指外側
-export const thumbOutLine = (ctx: CanvasRenderingContext2D): void => {
-  ctx.beginPath()
-  ctx.moveTo(165, 155)
-  ctx.quadraticCurveTo(180, 530, 470, 610)
-  // 親指ヘリ革
-  // ctx.moveTo(175, 142)
-  // ctx.quadraticCurveTo(190, 530, 470, 600)
-  ctx.stroke()
-}
-
-// 親指キリハミ1
-export const thumbEdgeLine1 = (ctx: CanvasRenderingContext2D): void => {
-  ctx.beginPath()
-  ctx.moveTo(225, 120)
-  ctx.quadraticCurveTo(250, 440, 435, 550)
-  ctx.stroke()
-}
-
-// 親指キリハミ2
-export const thumbEdgeLine2 = (ctx: CanvasRenderingContext2D): void => {
-  ctx.beginPath()
-  ctx.moveTo(435, 550)
-  ctx.quadraticCurveTo(475, 585, 472, 598)
   ctx.stroke()
 }
 
@@ -55,4 +20,40 @@ export const thumbInLine2 = (ctx: CanvasRenderingContext2D): void => {
   ctx.moveTo(470, 510)
   ctx.quadraticCurveTo(490, 560, 505, 605)
   ctx.stroke()
+}
+
+// 親指マチ部分
+export const thumbMachi = (ctx: CanvasRenderingContext2D, color: string): void => {
+  ctx.lineWidth = 2
+  ctx.strokeStyle = '#red'
+  ctx.fillStyle = color
+
+  ctx.beginPath()
+  ctx.moveTo(175, 130)
+  ctx.quadraticCurveTo(188, 530, 470, 595)
+  ctx.quadraticCurveTo(450, 560, 410, 535)
+  ctx.quadraticCurveTo(255, 450, 225, 120)
+  ctx.lineTo(175, 130)
+  ctx.stroke()
+  ctx.fill()
+  ctx.closePath()
+}
+
+// 親指ターゲット部分
+export const thumbTarget = (ctx: CanvasRenderingContext2D, color: string): void => {
+  ctx.lineWidth = 2
+  ctx.strokeStyle = '#red'
+  ctx.fillStyle = color
+
+  ctx.beginPath()
+  ctx.moveTo(225, 120)
+  ctx.quadraticCurveTo(265, 460, 410, 535)
+  ctx.quadraticCurveTo(430, 545, 470, 595)
+  ctx.quadraticCurveTo(490, 602, 503, 600)
+  ctx.lineTo(470, 510)
+  ctx.quadraticCurveTo(325, 410, 275, 170)
+  ctx.quadraticCurveTo(250, 130, 225, 120)
+  ctx.stroke()
+  ctx.fill()
+  ctx.closePath()
 }
