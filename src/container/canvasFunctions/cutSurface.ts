a03ac561de7e155c8bc7363f17ba7c9d4699f239
tomoki33kurita <tomoki33kurita@gmail.com>
2021-01-03 12:13:54 +0900

    update little edge position

--- src/container/canvasFunctions/cutSurface.ts
+++ src/container/canvasFunctions/cutSurface.ts
@@ -20,15 +20,15 @@ export const littleCutSurface = (ctx: CanvasRenderingContext2D, color: string): 
   ctx.strokeStyle = '#383838'
   ctx.fillStyle = color
   ctx.beginPath()
-  ctx.moveTo(720, 153)
-  ctx.quadraticCurveTo(726, 170, 726, 181)
+  ctx.moveTo(718, 153)
+  ctx.quadraticCurveTo(726, 170, 725, 181)
   ctx.quadraticCurveTo(786, 390, 649, 511)
   ctx.quadraticCurveTo(644, 521, 646, 555)
   ctx.quadraticCurveTo(651, 560, 653, 553)
   ctx.quadraticCurveTo(649, 534, 656, 515)
   ctx.quadraticCurveTo(793, 389, 733, 180)
-  ctx.quadraticCurveTo(729, 164, 729, 164)
-  ctx.quadraticCurveTo(720, 145, 720, 153)
+  ctx.quadraticCurveTo(729, 164, 727, 164)
+  ctx.quadraticCurveTo(720, 145, 718, 153)
 
   ctx.fill()
   ctx.stroke()
