bdcc1a6df0606d3fd824985ed809850d2867e3dd
tomoki33kurita <tomoki33kurita@gmail.com>
2021-02-20 17:02:31 +0900

    Moved confirm btn

--- src/container/FigureFields.tsx
+++ src/container/FigureFields.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Box, AppBar, Tabs, Tab, Button } from '@material-ui/core'
+import { Box, AppBar, Tabs, Tab } from '@material-ui/core'
 import TabPanel from 'src/components/molecules/TabPanel'
 import { State } from 'src/types'
-import PdfDialog from 'src/container/PdfDialog'
 import FigureFront from 'src/container/FigureFront'
 import FigureBack from 'src/container/FigureBack'
 
@@ -20,13 +19,8 @@ type Props = {
 }
 
 const FigureFields: React.FC<Props> = ({ state }) => {
-  // todo あとでuseReducerを突っ込んでまとめて状態管理する
   const [value, setValue] = React.useState(1)
   const handleChange = (event: any, newValue: number) => setValue(newValue)
-  const [open, setOpen] = React.useState<boolean>(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
-
   //// this is devTools
   // const [cordinateX, setCordinateX] = React.useState(0)
   // const [cordinateY, setCordinateY] = React.useState(0)
@@ -44,7 +38,6 @@ const FigureFields: React.FC<Props> = ({ state }) => {
 
   return (
     <Box position="sticky" top={0}>
-      <PdfDialog state={state} open={open} handleClose={handleClose} />
       <TabPanel value={value} index={0}>
         <FigureFront
           parts={{
@@ -105,12 +98,6 @@ const FigureFields: React.FC<Props> = ({ state }) => {
           <Tab label="背面" {...a11yProps(1)} />
         </Tabs>
       </AppBar>
-      <Box display="flex" justifyContent="space-around" mt={5}>
-        <Button variant="outlined">リセット</Button>
-        <Button variant="contained" color="primary" onClick={handleOpen}>
-          オーダー内容確認
-        </Button>
-      </Box>
     </Box>
   )
 }
